d420203668e16e36947660871ac461fe95aacd14
Lewis Brown <75475793+lbrown65@users.noreply.github.com>
2021-07-08 11:39:48 +0100

    hwh file location made more generic
    hwh file moved for 2020.2, changed method to get_files based - will now always find correct location.

--- board/RFSoC2x2/base/build_bitstream.tcl
+++ board/RFSoC2x2/base/build_bitstream.tcl
@@ -34,4 +34,7 @@ validate_hw_platform ./${overlay_name}.xsa
 file copy -force ./${overlay_name}/${overlay_name}.runs/impl_1/${design_name}_wrapper.bit ${overlay_name}.bit
 
 # copy hwh files
-file copy -force ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/hw_handoff/${design_name}.hwh ${overlay_name}.hwh
+set bd_file [get_files "${overlay_name}.bd"]
+set hwh_file [get_files -of_objects $bd_file "${overlay_name}.hwh"]
+file copy $hwh_file ${overlay_name}.hwh
+
